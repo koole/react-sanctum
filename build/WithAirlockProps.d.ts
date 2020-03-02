002d0c99d2a9a00dcc2f8b8876fae7300acf0969
Leon Koole <leon@koole.io>
2020-03-02 14:54:34 +0100

    Working esm build

--- build/WithAirlockProps.d.ts
+++
@@ -1,7 +0,0 @@
-export default interface WithAirlockProps<UserObj> {
-    user: null | UserObj;
-    authenticated: null | boolean;
-    signIn: (email: string, password: string) => Promise<{}>;
-    signOut: () => void;
-    checkAuthentication: () => Promise<boolean>;
-}
--- rollup.config.js
+++ rollup.config.js
@@ -1,47 +1,46 @@
-import typescript from "rollup-plugin-typescript2";
-import commonjs from "rollup-plugin-commonjs";
-import external from "rollup-plugin-peer-deps-external";
-import resolve from "rollup-plugin-node-resolve";
+
 import json from "@rollup/plugin-json";
-
+import typescript from "rollup-plugin-typescript2";
 import pkg from "./package.json";
+// import resolve from "rollup-plugin-node-resolve";
+// import commonjs from "rollup-plugin-commonjs";
+// import externalGlobals from "rollup-plugin-external-globals";
 
-export default {
-  input: "src/index.ts",
-  output: [
-    {
-      file: pkg.main,
-      format: "cjs",
-      exports: "named",
-      sourcemap: true
-    },
-    {
-      file: pkg.module,
-      format: "es",
-      exports: "named",
-      sourcemap: true
-    }
-  ],
-  plugins: [
-    external(),
-    resolve(),
-    typescript({
-      rollupCommonJSResolveHack: true,
-      clean: true
-    }),
-    json(),
-    commonjs({
-      include: ["node_modules/**"],
-      namedExports: {
-        "node_modules/react/react.js": [
-          "Children",
-          "Component",
-          "PropTypes",
-          "createElement",
-          "createContext"
-        ],
-        "node_modules/react-dom/index.js": ["render"]
-      }
-    })
-  ]
-};
+export default [
+  // browser-friendly UMD build
+  //! Currently broken, so isn't included in package.json
+  //   {
+  //     input: "src/index.ts",
+  //     output: {
+  //       name: "react-airlock",
+  //       file: pkg.browser,
+  //       format: "umd"
+  //     },
+  //     external: ["react", "react-dom"],
+  //     plugins: [
+  //       resolve({ browser: true }),
+  //       json(),
+  //       commonjs(),
+  //       typescript(),
+  //       externalGlobals({
+  //         react: "React"
+  //       })
+  //     ]
+  //   },
+
+  // CommonJS (for Node) and ES module (for bundlers) build.
+  // (We could have three entries in the configuration array
+  // instead of two, but it's quicker to generate multiple
+  // builds from a single configuration where possible, using
+  // an array for the `output` option, where we can specify
+  // `file` and `format` for each target)
+  {
+    input: "src/index.ts",
+    external: ["react", "axios", "hoist-non-react-statics", "tiny-invariant"],
+    plugins: [json(), typescript()],
+    output: [
+      { file: pkg.main, format: "cjs", sourcemap: true },
+      { file: pkg.module, format: "es", sourcemap: true }
+    ]
+  }
+];
