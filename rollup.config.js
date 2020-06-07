03919212c5b464e10da4cdda5044ebcbb2f0604a
Leon Koole <leon@koole.io>
2020-06-07 15:46:51 +0200

    Update and clean up dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -2,32 +2,8 @@
 import json from "@rollup/plugin-json";
 import typescript from "rollup-plugin-typescript2";
 import pkg from "./package.json";
-// import resolve from "rollup-plugin-node-resolve";
-// import commonjs from "rollup-plugin-commonjs";
-// import externalGlobals from "rollup-plugin-external-globals";
 
 export default [
-  // browser-friendly UMD build
-  //! Currently broken, so isn't included in package.json
-  //   {
-  //     input: "src/index.ts",
-  //     output: {
-  //       name: "react-sanctum",
-  //       file: pkg.browser,
-  //       format: "umd"
-  //     },
-  //     external: ["react", "react-dom"],
-  //     plugins: [
-  //       resolve({ browser: true }),
-  //       json(),
-  //       commonjs(),
-  //       typescript(),
-  //       externalGlobals({
-  //         react: "React"
-  //       })
-  //     ]
-  //   },
-
   // CommonJS (for Node) and ES module (for bundlers) build.
   // (We could have three entries in the configuration array
   // instead of two, but it's quicker to generate multiple
