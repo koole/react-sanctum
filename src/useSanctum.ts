6ae3dbc74f0bc1090a74a1c665b87874c884a370
Leon Koole <leon@koole.io>
2021-08-13 15:26:11 +0200

    improve useSanctum

--- src/useSanctum.ts
+++ src/useSanctum.ts
@@ -3,7 +3,10 @@ import * as React from "react";
 import SanctumContext from "./SanctumContext";
 
 const useSanctum = () => {
-  return React.useContext(SanctumContext);
+  const context = React.useContext(SanctumContext);
+  if (!context)
+    throw new Error("useSanctum should only be used inside <Sanctum />");
+  return context;
 };
 
 export default useSanctum;
